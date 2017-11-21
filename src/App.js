fb02c980ba0b7e1d520d0e8a583f6b6295394602
Sibi Prabakaran <sibi@psibi.in>
2017-11-21 15:08:17 +0530

    Basic PS + React + Creat React App working code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import Hello from "./Hello";
+import { helloWorld, kdfaj } from "./ps/output/HelloWorld";
 import "./App.css";
 
 class App extends Component {
@@ -14,7 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Hello />
+        <p>ja</p>
+        {helloWorld()}
       </div>
     );
   }
---
+++ src/ps/src/HelloWorld.purs
@@ -0,0 +1,24 @@
+module HelloWorld where
+
+import Prelude
+import React.DOM as D
+import Control.Monad.Eff (Eff)
+import DOM (DOM())
+import DOM.HTML (window)
+import DOM.HTML.Types (htmlDocumentToDocument)
+import DOM.HTML.Window (document)
+import DOM.Node.NonElementParentNode (getElementById)
+import DOM.Node.Types (Element, ElementId(..), documentToNonElementParentNode)
+import React (createClass, spec, createFactory, ReactClass, ReactElement, createClassStateless)
+import Partial.Unsafe (unsafePartial)
+import Data.Maybe (fromJust, Maybe(..))
+import Data.Nullable (toMaybe)
+import ReactDOM (render)
+import React
+
+helloWorld :: ReactClass Unit
+helloWorld = createClassStateless helloText
+  where
+    helloText :: Unit -> ReactElement
+    helloText _ = D.h1 [] [D.text "hello dfkajk world"]
+
